46652c50c1d43c16bdcb670a249a3ca389392c9d
dave50b <57051618+dave50b@users.noreply.github.com>
2023-11-29 23:21:50 -0500

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -2,4 +2,5 @@
 /usr/bin/sudo pkill -F /home/pi/offpid.pid
 #/usr/bin/sudo pkill -F /home/pi/METARMap/metarpid.pid
 /usr/bin/sudo pkill -F /home/pi/metarpid.pid
-/usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/metar.py & echo $! > /home/pi/metarpid.pid
+#/usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/metar.py & echo $! > /home/pi/metarpid.pid
+/usr/bin/sudo /usr/bin/python3 /home/pi/metar.py & echo $! > /home/pi/metarpid.pid
